44bef43fda4ea4b9087af7f80e504aabae6defdb
Ionel Condor <ionel-condor@yahoo.com>
2015-12-21 18:06:18 +0200

    some messages

--- src/main/java/org/fasttrackit/dev/todolist/servlet/LoginServlet.java
+++ src/main/java/org/fasttrackit/dev/todolist/servlet/LoginServlet.java
@@ -16,20 +16,42 @@ public class LoginServlet extends HttpServlet {
 
     public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        // read user and password introduced by the user in the UI
+        String user = request.getParameter("u");
+        String passwd = request.getParameter("p");
 
-        // db
+        // static & simulated db row
         final String dbu="ionel";
         final String dbp="anaaremere";
         final String userid="356";
 
-        // citesc din db select user,password from dbuser
-        // pun result set-ul intr-o colectie de beanuri de tip user pasword
+
+        // version 2
+        // pls notice both the user and password are submitted in the address bar, is this correct?
+        // TASK: fix it
+
+        // version 3
+        // let's imagine the row is actually part of a list of users
+        // (  (user1,password1,id1)(user2,password2,id2)(user3,password3, id3).... )
+        // TASK: change the implementation above in such a way that the user and password read from UI is compared against
+        // the content of a collection
 
 
-        String user = request.getParameter("u");
-        String passwd = request.getParameter("p");
+        // version 4
 
-        if(dbu.equals(user) && dbp.equals(passwd)) { // itere in colectie si compar
+        // TASK: read this from a real db:
+        // a) select user,password from dbuser
+        // b) put the result set in the collection created at version 2
+        // c) iterate and compare
+
+
+
+        // version 5
+        // the UI and the usability is so ugly
+        // TASK: pls fix the UI and the usability by adding some css classes
+
+
+        if(dbu.equals(user) && dbp.equals(passwd)) {
             // userul exista in db
 
 
